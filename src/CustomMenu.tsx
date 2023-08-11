58005b742e553a86028a8c7c4d5ee879b862b264
isazobu <48869172+isazobu@users.noreply.github.com>
2023-08-11 13:55:44 +0300

    Update CustomMenu.tsx

--- src/CustomMenu.tsx
+++ src/CustomMenu.tsx
@@ -1,6 +1,6 @@
 import { Menu } from 'react-admin';
 import {MapOutlined} from '@mui/icons-material';
-
+import AllOutIcon from '@mui/icons-material/AllOut';
 export const CustomMenu = () => (
     <Menu>
         <Menu.DashboardItem />
@@ -8,5 +8,7 @@ export const CustomMenu = () => (
         {/* <Menu.ResourceItem name="geo" /> */}
         <Menu.ResourceItem name="weapons" />
         <Menu.Item to="/geo" primaryText="Geo" leftIcon={<MapOutlined />}/>
+        <Menu.Item to="/radial-menu" primaryText="Radial Menu Test" leftIcon={<AllOutIcon />} />
+        <Menu.Item to="/fan-menu" primaryText="Static Fan Menu Test" leftIcon={<AllOutIcon />} />
     </Menu>
 );
